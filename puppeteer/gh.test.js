5aebd2abad541b996179cd6ef236adf6a10115ec
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2023-05-23 17:12:10 +0300

    hw4

---
+++ puppeteer/gh.test.js
@@ -0,0 +1,86 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  }, 10000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  }, 20000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  }, 10000);
+});
+
+describe("Github page pricing", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/pricing");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Pricing · Plans for every developer · GitHub');
+  }, 10000);
+
+});
+
+describe("Github page enterprise", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/enterprise");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Enterprise · A smarter way to work together · GitHub');
+  }, 20000);
+
+});
+
+describe("Github page enterprise", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/features");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Features | GitHub · GitHub');
+  }, 40000);
+
+});
